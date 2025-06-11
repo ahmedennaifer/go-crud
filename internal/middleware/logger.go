46693aa02d93c09d3da21b94a94a3bbe851e0131
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-06-11 16:30:11 +0200

    feat: dummy log to logfile

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -1,13 +1,31 @@
 package middleware
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
+
+const filePath = "./server.log" // TODO:: read from config
 
 type ResponseWriterWrapper struct {
 	http.ResponseWriter
 	statusCode int
+}
+
+func writeLog(pattern, file string) error {
+	f, err := os.OpenFile(file, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		fmt.Print(err)
+		return err
+	}
+	defer f.Close()
+	if _, err = f.WriteString(pattern); err != nil {
+		fmt.Print(err)
+	}
+	return nil
 }
 
 func (rw *ResponseWriterWrapper) WriteHeader(code int) {
@@ -23,6 +41,8 @@ func LoggingMiddleware(handler http.HandlerFunc) http.HandlerFunc {
 		}
 		handler(rw, r)
 		log.Printf("%v %v %v", rw.statusCode, r.Method, r.URL.Path)
+		pattern := fmt.Sprintf("%v %v %v %v\n", time.Now().Format(time.ANSIC), rw.statusCode, r.Method, r.URL.Path)
+		writeLog(pattern, filePath)
 
 	}
 }
